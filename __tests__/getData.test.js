5dee6a5b03b5eafadbc7e7596e9d22261f12385e
hagooog <mohamed.higgy31@gmail.com>
2021-05-21 22:37:40 +0200

    “updated”

--- __tests__/getData.test.js
+++ __tests__/getData.test.js
@@ -15,9 +15,26 @@ test("check array don't have number 20", () => {
     expect(getData).not.toContain(20);
 });
 
-//not.toContain() check if number in array
+//not.toBe() check if number in array
 test("check array don't have 0", () => {
     for (let index = 0; index < getData.length; index++) {
         expect(getData[index]).not.toBe(10);
     }
+});
+
+//toBeFalsy() check if all index in array is number
+test("check array don't have 0", () => {
+    for (let index = 0; index < getData.length; index++) {
+        expect(isNaN(getData[index])).toBeFalsy();
+    }
+});
+
+//toBeGreaterThanOrEqual() check if first number in array GreaterThanOrEqual
+test("check first elemnt reaterThanOrEqual 1", () => {
+    expect(getData[0]).toBeGreaterThanOrEqual(1);
+});
+
+//toBeLessThanOrEqual() check if first number in array LessThanOrEqual
+test("check first elemnt LessThanOrEqual 101", () => {
+    expect(getData[0]).toBeLessThanOrEqual(10);
 });
